642ec53587cf3cc90c7b160f634ba3115d6eed55
Andres Moreno <feliandresmorepi@gmail.com>
2025-03-18 19:30:12 -0500

    ajuste de precios de string a integer (cambios en la forma en que se muestra)

--- productos/articulos.js
+++ productos/articulos.js
@@ -3,14 +3,14 @@ const articulos = [
     id:1,
     img:"imagenes/productos/1.png",
     nombre: "Audifonos A-37 PRO",
-    precio:"40.000",
+    precio:45000,
 },
 
 {
     id:2,
     img:"imagenes/productos/2.png",
     nombre: "Audifonos Inalambricos",
-    precio:"60.800",
+    precio:60800,
 },
 
 
@@ -18,40 +18,40 @@ const articulos = [
     id:3,
     img:"imagenes/productos/3.png",
     nombre: "audifonos vsg singularity",
-    precio:"150.000",
+    precio:150000,
 },
 
 {
     id:4,
     img:"imagenes/productos/4.png",
     nombre: "Audifonos inalambricos rosados",
-    precio:"45.000",
+    precio:40000,
 },
 
 {
     id:5,
     img:"imagenes/productos/5.png",
     nombre: "Nintendo switch",
-    precio:"1.550.000",
+    precio:1550000,
 },
 
 {
     id:6,
     img:"imagenes/productos/6.png",
     nombre: "xiaomi Redmi 14C",
-    precio:"450.000",
+    precio:450000,
 },
 {
     id:7,
     img:"imagenes/productos/7.png",
     nombre: "PS4 Slim",
-    precio:"1.600.000",
+    precio:1600000,
 },
 {
     id:8,
     img:"imagenes/productos/8.png",
     nombre: "Redmi Pad SE",
-    precio:"550.000",
+    precio:550000,
 },
 
 
