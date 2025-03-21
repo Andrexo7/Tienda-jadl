ce675193c2631023c2d6b605d70ac8b0014b6809
Andres Moreno <feliandresmorepi@gmail.com>
2025-03-20 20:14:06 -0500

    modificaciones para implementacion menu de  categorias

--- productos/articulos.js
+++ productos/articulos.js
@@ -4,6 +4,7 @@ const articulos = [
     img:"imagenes/productos/1.png",
     nombre: "Audifonos A-37 PRO",
     precio:45000,
+    categoria:"Audio"
 },
 
 {
@@ -11,14 +12,16 @@ const articulos = [
     img:"imagenes/productos/2.png",
     nombre: "Audifonos Inalambricos",
     precio:60800,
+    categoria:"Audio"
 },
 
 
 {
     id:3,
     img:"imagenes/productos/3.png",
-    nombre: "audifonos vsg singularity",
+    nombre: "Audifonos vsg singularity",
     precio:150000,
+    categoria:"Audio"
 },
 
 {
@@ -26,6 +29,7 @@ const articulos = [
     img:"imagenes/productos/4.png",
     nombre: "Audifonos inalambricos rosados",
     precio:40000,
+    categoria:"Audio"
 },
 
 {
@@ -33,6 +37,7 @@ const articulos = [
     img:"imagenes/productos/5.png",
     nombre: "Nintendo switch",
     precio:1550000,
+    categoria:"Consolas"
 },
 
 {
@@ -40,36 +45,42 @@ const articulos = [
     img:"imagenes/productos/6.png",
     nombre: "xiaomi Redmi 14C",
     precio:450000,
+    categoria:"Celulares"
 },
 {
     id:7,
     img:"imagenes/productos/7.png",
     nombre: "PS4 Slim",
     precio:1600000,
+    categoria:"Consolas"
 },
 {
     id:8,
     img:"imagenes/productos/8.png",
     nombre: "Redmi Pad SE",
     precio:550000,
+    categoria:"Tablets"
 },
 {
     id:9,
     img:"imagenes/productos/9.png",
     nombre: "TV Samsung 55' pulgadas ",
     precio:1945000,
+    categoria:"TV"
 },
 {
     id:10,
     img:"imagenes/productos/10.png",
     nombre: "mini consola retro 400",
     precio:40000,
+    categoria:"Consolas"
 },
 {
     id:11,
     img:"imagenes/productos/11.png",
     nombre: "tv lg 50' pulgadas ",
     precio:1599900 ,
+    categoria:"TV"
 },
 
 
